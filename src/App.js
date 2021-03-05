158192214708b8558e3fcdfef545f64412e9669a
majazocom <maja.gedda@zocom.se>
2021-03-05 15:24:51 +0100

    scss test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>This is a heading</h1>
+          <h2>This is a heading</h2>
+        </header>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is a heading</h1>
       </header>
+      <article className="monkey"></article>
+      <button className="btn primary-btn danger">KNAPP</button>
+      <button className="btn secondary-btn success">KNAPP</button>
+      <button className="btn primary-btn success">KNAPP</button>
+      <button className="btn primary-btn success">KNAPP</button>
     </div>
   );
 }
